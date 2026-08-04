a3cbc204b6b85d2993eafe59b65cd283eb9262cd
AFI CAFE <aficafeec@gmail.com>
2026-08-03 20:01:09 -0500

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // para desarrollo local. Antes de publicar en GitHub Pages, cambia esto a "/afi-cafe/"
+  base: "/afi-cafe/",
 });
